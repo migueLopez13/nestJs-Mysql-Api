b5581d11316ecaac95a2d17181e72a13a9e626c3
miguelopez13 <mlopari216@g.educaand.es>
2022-01-20 10:22:57 +0100

    build user.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+export interface UserApi {
+  userId: string;
+  username: string;
+  password: string;
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 'dsfsdfs-sadfsdf-sdfsdf',
+      username: 'politecnico',
+      password: 'pruebanestjs',
+    },
+  ];
+
+  async findOne(username: string): Promise<UserApi | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
