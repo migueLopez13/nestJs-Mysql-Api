55908456c9d9cde43b0a1c5fc4f9bfd2be235f29
migueLopez13 <90908260+migueLopez13@users.noreply.github.com>
2022-01-21 14:15:07 +0100

    Delete dist directory

--- dist/auth/auth.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { UsersService } from '../users/users.service';
-export declare class AuthService {
-    private usersService;
-    constructor(usersService: UsersService);
-    validateUser(username: string, pass: string): Promise<any>;
-}
