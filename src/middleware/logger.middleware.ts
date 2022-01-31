61587c92db636dafcbed8ccc5c2cb778e6b6a5ad
miguelopez13 <mlopari216@g.educaand.es>
2022-01-31 14:24:45 +0100

    build middleware

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,6 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-
+//TODO
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
