1403f8ef0917736c83ef283360a5ab4ad0adfe12
miguelopez13 <mlopari216@g.educaand.es>
2022-02-01 09:20:11 +0100

    add handler

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ErrorsInterceptor } from './common/interceptors/errors.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  //app.useGlobalInterceptors(new ErrorsInterceptor());
+  app.useGlobalInterceptors(new ErrorsInterceptor());
   await app.listen(3000);
 }
 bootstrap();
